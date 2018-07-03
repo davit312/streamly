1713a5881ac7d317030d87883e2d90754ac82129
davi807 <davi807plus@yandex.ru>
2018-07-03 22:19:08 +0400

    parse

--- streamly.go
+++ streamly.go
@@ -2,37 +2,31 @@ package streamly
 
 import (
 	"bufio"
-	"io"
 	"log"
 	"net/http"
 	"strings"
 )
 
-func Parse(addr string, f io.Writer) []string {
+func Parse(addr string) []string {
 	data, err := http.Get(addr)
 	if err != nil {
 		log.Print(err)
 		return nil
 	}
 
+	result := make([]string, 8)
 	reader := bufio.NewReader(data.Body)
-	for {
+	for i := 0; i < 8; {
 		r, _, e := reader.ReadLine()
 		if e != nil {
 			log.Print(e)
-			return nil
+			break
 		}
-
 		s := string(r)
-
 		if !strings.ContainsRune(s, '#') {
-			data, er := http.Get("http://amtv1.livestreamingcdn.com/am1abr/tracks-v2a1/" + s)
-			if er != nil {
-				continue
-			}
-			io.Copy(f, data.Body)
+			result[i] = s
+			i++
 		}
 	}
-
-	return nil
+	return result
 }
