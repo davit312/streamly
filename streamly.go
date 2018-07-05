c2c69b08577708ed4d678693b865ab36f6dec8cf
davi807 <davi807plus@yandex.ru>
2018-07-05 07:53:51 +0400

    simple history

--- streamly.go
+++ streamly.go
@@ -2,30 +2,53 @@ package streamly
 
 import (
 	"bufio"
-	"log"
 	"net/http"
 	"strings"
 )
 
-func Parse(addr string) []string {
-	data, err := http.Get(addr)
+func getM3u(url string) *bufio.Reader {
+	conn, err := http.Get(url)
 	if err != nil {
-		log.Print(err)
 		return nil
 	}
+	return bufio.NewReader(conn.Body)
+}
 
+func Parse(addr string, last *[8]string) []string {
+
+	i := 0
+	l := 0
+	isNew := false
+
+	reader := getM3u(addr)
 	result := make([]string, 8)
-	reader := bufio.NewReader(data.Body)
-	for i := 0; i < 8; {
+
+	for i < 8 {
+	begin:
 		r, _, e := reader.ReadLine()
 		if e != nil {
-			log.Print(e)
 			break
 		}
 		s := string(r)
 		if !strings.ContainsRune(s, '#') {
+			if !isNew {
+				isNew = true
+				for _, item := range last {
+					if item == s {
+						isNew = false
+						goto begin
+					}
+				}
+			}
+
 			result[i] = s
 			i++
+
+			if l == 8 {
+				l = 0
+			}
+			last[l] = s
+			l++
 		}
 	}
 	return result
