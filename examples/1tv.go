2809aea056556741380a50ebadb4335d4c2830e1
davi807 <davi807plus@yandex.ru>
2018-07-21 22:55:38 +0400

    1tv simple util

---
+++ examples/1tv.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"streamly"
+	"time"
+	"flag"
+	"log"
+)
+
+func main() {
+
+	playlist := flag.String("list", "", "playlist url")
+	startAt := flag.String("start", "0", "start record at minute")
+	prefix := flag.String("prefix", "http://amtv1.livestreamingcdn.com/am1abr/tracks-v2a1/", "source to download video chunks")
+	fileName := flag.String("out", "out", "output file address")
+    flag.Parse()
+
+	if(len(*playlist) < 1){
+		log.Fatal("Error: no playlist.")
+	}
+
+
+	skip, err := time.ParseDuration(*startAt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	
+	f, e := os.Create(*fileName)
+	if e != nil {
+		log.Fatal(e)
+	}
+	list := streamly.ParseRecord(*playlist, skip)
+	streamly.WriteAll(f, *prefix, list)
+}
--- examples/record.go
+++ examples/record.go
@@ -12,6 +12,6 @@ func main() {
 	if e != nil {
 		return
 	}
-	list := streamly.ParseRecord("http://amtv1.livestreamingcdn.com/am1abr/tracks-v2a1/index-1531414800-7800.m3u8", 10*time.Minute)
+	list := streamly.ParseRecord("http://amtv1.livestreamingcdn.com/am1abr/tracks-v2a1/index-1531512000-6000.m3u8", 0*time.Minute)
 	streamly.WriteAll(f, "http://amtv1.livestreamingcdn.com/am1abr/tracks-v2a1/", list)
 }
